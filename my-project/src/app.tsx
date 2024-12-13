7e9242beaabb3bd9d77f01935e826a61779b61d4
TaizaReis <taizareis16@gmail.com>
2024-12-13 04:58:54 -0300

    Atualizações em arquivos diversos, incluindo ajustes nos componentes e funções

--- my-project/src/app.tsx
+++ my-project/src/app.tsx
@@ -1,23 +1,28 @@
 //JSX - JavaScript XML -> HTML
+// import { useEffect, useState } from 'react'
 import { Dialog } from './componentes/ui/dialog'
-// import { EmptyGoals } from './componentes/empty-goals'
 import { CreateGoal } from './componentes/create-goal'
 import { Summary } from './componentes/summary'
-
+import { EmptyGoals } from './componentes/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,  //60 segundos
+  })
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
-
+      {data?.total  && data.total > 0 ? <Summary /> : <EmptyGoals />}
+      
       <CreateGoal />      
       
     </Dialog>
-
-  )
-  
+  )  
 }
 
 
--- my-project/src/main.tsx
+++ my-project/src/main.tsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import './index.css'
 
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
+  </QueryClientProvider>
+  
 )
